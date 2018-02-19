36d897ebe644ab437e3f7ec0d3c6abee411ec0dd
Bram <contact@bramvandenakker.nl>
2018-02-19 13:45:52 +0100

    reduced waittime to 15 minutes

--- preprocessing/highlightGenerator/kill-stuck-process.sh
+++ preprocessing/highlightGenerator/kill-stuck-process.sh
@@ -2,7 +2,7 @@
 lastfile=`ls ~/Human-Saliency-on-Snapshots-in-Web-Relevance/preprocessing/highlightGenerator/storage/highlights -t | head -1`
 echo $lastfile
 
-if test `find ~/Human-Saliency-on-Snapshots-in-Web-Relevance/preprocessing/highlightGenerator/storage/highlights/$lastfile -mmin +30`
+if test `find ~/Human-Saliency-on-Snapshots-in-Web-Relevance/preprocessing/highlightGenerator/storage/highlights/$lastfile -mmin +15`
 then
     killall firefox
     killall geckodriver
