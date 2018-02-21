4c252b9a3881d45442ab624151ac44bbdc58a62a
Bram van den Akker <git@bramvandenakker.nl>
2018-02-21 14:31:23 +0100

    updated readme

--- preprocessing/highlightGenerator/setup.sh
+++ preprocessing/highlightGenerator/setup.sh
@@ -5,8 +5,13 @@ mkdir storage/masks
 
 pip3 install -r requirements.txt
 
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
-tar xvjf phantomjs-2.1.1-linux-x86_64.tar.bz2
-sudo cp phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/bin/
-rm -r phantomjs-2.1.1-linux-x86_64
-rm phantomjs-2.1.1-linux-x86_64.tar.bz2
+# wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+# tar xvjf phantomjs-2.1.1-linux-x86_64.tar.bz2
+# sudo cp phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/bin/
+# rm -r phantomjs-2.1.1-linux-x86_64
+# rm phantomjs-2.1.1-linux-x86_64.tar.bz2
+
+wget https://github.com/mozilla/geckodriver/releases/download/v0.19.1/geckodriver-v0.19.1-linux64.tar.gz
+tar -zxvf geckodriver-v0.19.1-linux64.tar.gz
+rm geckodriver-v0.19.1-linux64.tar.gz
+sudo mv geckodriver /usr/bin/geckodriver
