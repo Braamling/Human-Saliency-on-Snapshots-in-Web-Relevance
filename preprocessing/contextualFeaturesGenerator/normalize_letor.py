1909215164bc77dda3857e7509a9b1053dd0d9d4
Bram van den Akker <git@bramvandenakker.nl>
2018-03-29 10:54:18 +0200

    calculate the content scores notebook

--- preprocessing/contextualFeaturesGenerator/normalize_letor.py
+++ preprocessing/contextualFeaturesGenerator/normalize_letor.py
@@ -34,8 +34,8 @@ def normalize_features(source_path, target_path):
     with open(target_path, "w") as f:
         for query in queries:
             features = np.asarray(queries[query][2])
-            norm_features = np.log(features + 1e-7)
-            norm_features = normalize(norm_features, axis=0)
+            # norm_features = np.log(features + 1e-7)
+            norm_features = normalize(features, axis=0)
             for i, (doc_id, rel_score) in enumerate(zip(queries[query][0], queries[query][1])):
                 start = "{} qid:{}".format(rel_score, query)
                 scores = " ".join([str(i) + ":" + '{0:.10f}'.format(x) for i,x in enumerate(norm_features[i,:])])
@@ -50,10 +50,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--source', type=str, default='storage/clueweb12_web_trec/full_set',
-                        help='The location of the content that should be added.')
-    parser.add_argument('--normalized_target', type=str, default='storage/clueweb12_web_trec/Querylevelnorm',
+                        help='The location of the LETOR file that should be added.')
+    parser.add_argument('--normalized_target', type=str, default='storage/clueweb12_web_trec/normalized_set',
                         help='The location where to store and load the context_features.')
 
     FLAGS, unparsed = parser.parse_known_args()
-
+    print(FLAGS)
     main()
